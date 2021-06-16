499fad5b5ae29cc70cba3f48d36a35a8f1dffae7
Erika Munini <erivimura@gmail.com>
2021-06-15 22:13:12 -0400

    Creando la estructura del proyecto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.svg';
+import './assets/css/App.css';
+
+function HolaMundo(nombre) {
+  var presentacion = (
+    <div>
+      <h2>Hola, soy {nombre}.</h2>
+    </div>
+  );
+  return presentacion;
+}
 
 function App() {
+  var nombre = "Erika Munini";
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +20,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {HolaMundo(nombre)}
         <a
           className="App-link"
           href="https://reactjs.org"
