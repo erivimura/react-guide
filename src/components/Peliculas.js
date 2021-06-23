67432a7a53ca3e96745d5fbf178d716e9087c905
Erika Munini <erivimura@gmail.com>
2021-06-22 23:04:24 -0400

    Probar states

--- src/components/Peliculas.js
+++ src/components/Peliculas.js
@@ -3,11 +3,32 @@ import MensajeEstatico from './MensajeEstatico';
 
 class Peliculas extends Component {
 
+    state = {
+        contador: 0
+    }
+
+    suma = (e) => {
+        this.setState({
+            contador: (this.state.contador + 1)
+        });        
+    }
+
+    resta = (e) => {
+        this.setState({
+            contador: (this.state.contador - 1)
+        }); 
+    }
+
     render() {
         return (
             <div id="peliculas">
                 <h4>Soy peli</h4>
                 <MensajeEstatico />
+                <p>Contador: {this.state.contador}</p>
+                <p>
+                    <button onClick={this.suma}>Suma</button>
+                    <button onClick={this.resta}>Resta</button>
+                </p>
             </div>            
         );
     }
