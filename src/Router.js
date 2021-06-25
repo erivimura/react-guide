31d08f0adcd229a5cbd6dcd27f7abe3819d7bf7f
Erika Munini <erivimura@gmail.com>
2021-06-24 23:30:42 -0400

    Error 404

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MiComponente from './components/MiComponente';
 import Peliculas from './components/Peliculas';
+import Error from './components/Error';
 
 class Router extends Component {
 
@@ -15,6 +16,8 @@ class Router extends Component {
                 <Switch>
                     <Route exact path="/" component={Peliculas} />  
                     <Route exact path="/ruta-prueba" component={MiComponente} />
+
+                    <Route component={Error} />
                 </Switch>
             
             </BrowserRouter>
